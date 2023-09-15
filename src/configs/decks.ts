fddb72cc5fefd87f3ed4be55f552f71a751d51e1
ixphinx <48696004+ixphinx@users.noreply.github.com>
2023-09-15 00:50:32 -0300

    2 files modified, 2 added

---
+++ src/configs/decks.ts
@@ -0,0 +1,86 @@
+export const decks = [
+  {
+    id: 0,
+    name: 'Aventuras de Thor',
+  },
+  {
+    id: 1,
+    name: 'Los Argonautas',
+  },
+  {
+    id: 2,
+    name: 'Bakedanuki',
+  },
+  {
+    id: 3,
+    name: 'Romulo y Remo',
+  },
+  {
+    id: 4,
+    name: 'Robin Hood',
+  },
+  {
+    id: 5,
+    name: 'Caballo de Troya',
+  },
+  {
+    id: 6,
+    name: 'Hilo del destino',
+  },
+  {
+    id: 7,
+    name: 'Viajes de Ulises',
+  },
+  {
+    id: 8,
+    name: 'Flautista de Hamelin',
+  },
+  {
+    id: 9,
+    name: 'El Doado',
+  },
+  {
+    id: 10,
+    name: 'La Carrera del Siglo',
+  },
+  {
+    id: 11,
+    name: 'La ciudad perdida',
+  },
+  {
+    id: 12,
+    name: 'Rey Arturo',
+  },
+  {
+    id: 13,
+    name: 'Paul Bunyan',
+  },
+  {
+    id: 14,
+    name: 'Nessie',
+  },
+  {
+    id: 15,
+    name: 'Triangulo de las Bermudas',
+  },
+  {
+    id: 16,
+    name: 'Hombre Lagarto',
+  },
+  {
+    id: 17,
+    name: 'Toque de Midas',
+  },
+  {
+    id: 18,
+    name: 'Bigfoot',
+  },
+  {
+    id: 19,
+    name: 'Acalica',
+  },
+  {
+    id: 20,
+    name: 'Hua Mulan',
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,21 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { decks } from './configs/decks';
 
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular
-    </a>
-  `,
+  templateUrl: './main.html',
 })
 export class App {
-  name = 'Angular';
+  decks = decks;
+  id!: number;
+
+  getId(id: number) {
+    this.id = id;
+  }
 }
 
 bootstrapApplication(App);
